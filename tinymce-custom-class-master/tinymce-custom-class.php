e577dd5a5484fd20ceebbf1dc46d4b22d5063720
nafsinvk <nafsinvk@gmail.com>
2017-11-02 14:50:36 +0300

    Minor correction

--- tinymce-custom-class-master/tinymce-custom-class.php
+++ tinymce-custom-class-master/tinymce-custom-class.php
@@ -86,7 +86,7 @@ function getForm()
 	</div>
 	<div class="nafs-form-el-wrap">
 	<label for="nafs-item-discription">Content</label>
-	<input type="text" name="nafs-item-discription" id="nafs-item-discription" class="form-input-tip">
+	<textarea name="nafs-item-discription" id="nafs-item-discription" class="form-input-tip"></textarea>
 	<div class="help">Enter the content to be shown inside accordion</div>
 	</div>
 	<div class="nafs-form-el-wrap">
@@ -135,24 +135,7 @@ function admin_footer_scripts() {
 		return;
 	}
 	?>
-	<script type="text/javascript">
-		QTags.addButton( 'custom_class', 'Insert Custom Class', insert_custom_class );
-		function insert_custom_class() {
-		    // Ask the user to enter a CSS class
-		    var result = prompt('Enter the CSS class');
-		    if ( !result ) {
-		        // User cancelled - exit
-		        return;
-		    }
-		    if (result.length === 0) {
-		        // User didn't enter anything - exit
-		        return;
-		    }
 
-		    // Insert
-		    QTags.insertContent('<span class="' + result +'"></span>');
-		}
-	</script>
 	<?php
 
 }
